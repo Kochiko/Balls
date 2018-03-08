107d2b384e757fa913d21173e7f9300a1a546a88
Kochiko <rodroesteban@gmail.com>
2018-03-08 11:23:07 +0300

    Поля для ввода текста Entry
    Добавлены окошки для ввода текста (нерабочие).
    Для размещения используется метод place.

--- Balls.py
+++ Balls.py
@@ -13,12 +13,16 @@ _1BALL_RADIUS = 10
 _2BALL_RADIUS = 10
 # устанавливаем окно
 root = Tk()
-root.title("PythonicWay Pong")
+root.title("Balls")
  
 # область анимации
 c = Canvas(root, width=WIDTH, height=HEIGHT, background="#003300")
 c.pack()
  
+
+# Перевменные вводимые в окошки
+speed_1 = ""
+speed_2 = ""
 
 # установка объектов
  
@@ -34,6 +38,20 @@ _2BALL = c.create_oval(WIDTH*0.75 - _1BALL_RADIUS/2,
                      WIDTH*0.75 + _1BALL_RADIUS/2,
                      HEIGHT/2 + _1BALL_RADIUS/2, fill="white")
  
- 
+
+# Создаем окошки
+speed1_entry = Entry(speed_1)
+speed2_entry = Entry(speed_2)
+
+speed1_entry.place(relx = .15, rely = .05, anchor = "e", height=30, width=130)
+speed2_entry.place(relx = .85, rely = .05, anchor = "w", height=30, width=130)
+
+# Записываем текст в окошках и переводим в число
+sp1 = int(speed1_entry.get())
+sp2 = int(speed2_entry.get())
+
+enter(sp1)
+enter(sp2)
+
 # запускаем работу окна
 root.mainloop()
